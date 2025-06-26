9bd3bf0cc6f300781044211e9e7db9186e0fdef0
Xai-3N1AL3 <xyronncahayon28@gmail.com>
2025-06-26 09:26:18 +0800

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,160 +1,129 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Profile()));
+  runApp(const MyApp());
 }
 
-class Profile extends StatelessWidget {
-  const Profile({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MemeFeedPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MemeFeedPage extends StatefulWidget {
+  const MemeFeedPage({super.key});
+
+  @override
+  State<MemeFeedPage> createState() => _MemeFeedPageState();
+}
+
+class _MemeFeedPageState extends State<MemeFeedPage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> memePosts = const [
+    MemePost(
+      username: "TigerTheThrid",
+      description:
+      "Little drawing I did for my friends (they gave me an image and I had to make a comic) :)",
+      icon: Icons.pets,
+    ),
+    MemePost(
+      username: "khanTahsinAbrar",
+      description:
+      "Here I am, most powerful human in the world, filled with love",
+      icon: Icons.sunny,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Profile',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.black,
-      ),
-      body: Container(
-        margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-        child: Column(
-          children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+      body: NestedScrollView(
+        headerSliverBuilder: (_, __) => [
+          SliverAppBar(
+            floating: true,
+            snap: true,
+            backgroundColor: Colors.white,
+            title: Row(
+              children: const [
+                Icon(Icons.emoji_emotions, color: Colors.purple),
+                SizedBox(width: 8),
                 Text(
-                  'User Profile',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                  ),
+                  "Meme Feed",
+                  style: TextStyle(color: Colors.black),
                 ),
               ],
             ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.person, size: 80),
-              ],
-            ),
-            Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.fromLTRB(30, 20, 0, 0),
-                  child: const Row(
-                    children: [
-                      Text(
-                        'Name:',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          fontFamily: 'Times New Roman',
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                      child: Text('Xyronn Cahayon'),
-                    ),
-                  ],
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(30, 10, 0, 0),
-                  child: const Row(
-                    children: [
-                      Text(
-                        'Email:',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          fontFamily: 'Times New Roman',
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                      child: Text('xyronncahayon28@gmail.com'),
-                    ),
-                  ],
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(30, 10, 0, 0),
-                  child: const Row(
-                    children: [
-                      Text(
-                        'Company:',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          fontFamily: 'Times New Roman',
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                      child: Text('None'),
-                    ),
-                  ],
-                ),
-                const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(30, 10, 0, 0),
-                      child: Text(
-                        'Contact Number:',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          fontFamily: 'Times New Roman',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                      child: Text('None'),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(0, 250, 0, 0),
-                      width: 300,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black,
-                        ),
-                        child: const Text(
-                          'Logout',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+            actions: const [
+              Icon(Icons.refresh, color: Colors.black),
+              SizedBox(width: 12),
+            ],
+          ),
+        ],
+        body: ListView.builder(
+          padding: const EdgeInsets.only(bottom: 80),
+          itemCount: memePosts.length,
+          itemBuilder: (context, index) => memePosts[index],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Feed",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: "Messages",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MemePost extends StatelessWidget {
+  final String username;
+  final String description;
+  final IconData icon;
+
+  const MemePost({
+    super.key,
+    required this.username,
+    required this.description,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            leading: CircleAvatar(child: Text(username[0])),
+            title: Text(username),
+            subtitle: Text(description),
+            trailing: const Icon(Icons.sentiment_neutral),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Icon(icon, size: 100, color: Colors.grey[700]),
+          ),
+        ],
       ),
     );
   }
